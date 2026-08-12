ee3a860d11c1ff1b8cfa65b27dfbc960077db468
Graziano <stella@grazianoderossi.it>
2026-08-13 01:47:43 +0200

    Update font-config.js

--- font-config.js
+++ font-config.js
@@ -1,5 +1,5 @@
 WebFont.load({
   google: {
-    families: ["Geist:100,200,300,400,500,900", "Changa One:400,400italic", "Inconsolata:400,700"],
+    families: ["Geist:100,200,300,400,500,900"],
   },
 });
